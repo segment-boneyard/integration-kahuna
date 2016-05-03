1c3d1f3e241d2710810879b12b81e5fa134d71c0
Han Kim <han@segment.com>
2016-05-02 17:06:23 -0700

    support push tokens and browser name

--- test/index.js
+++ test/index.js
@@ -86,6 +86,20 @@ describe('Kahuna', function(){
         .end(done);
     });
 
+    it('should send push token', function(done){
+      var json = test.fixture('identify-token');
+
+      json.output.key = settings.apiKey;
+      json.output.env = settings.env ? 'p' : 's';
+
+      var output = json.output;
+      test
+        .identify(json.input)
+        .sends(json.output)
+        .expects(200)
+        .end(done);
+    });
+
     it('identify should have dev_id', function(done){
       var json = test.fixture('identify-no-dev-id');
 
@@ -98,6 +112,62 @@ describe('Kahuna', function(){
         .sends(json.output)
         .expects(200)
         .end(done);
+    });
+
+    it('should map useragent for firefox', function(done){
+      var json = test.fixture('identify-useragent');
+      json.input.context.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:40.0) Gecko/20100101 Firefox/40.1';
+      json.output.os_name = 'firefox';
+      json.output.key = settings.apiKey;
+      json.output.env = settings.env ? 'p' : 's';
+
+      test
+        .identify(json.input)
+        .sends(json.output)
+        .expects(200)
+        .end(done)
+    });
+
+    it('should map useragent for chrome', function(done){
+      var json = test.fixture('identify-useragent');
+      json.input.context.userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36';
+      json.output.os_name = 'chrome';
+      json.output.key = settings.apiKey;
+      json.output.env = settings.env ? 'p' : 's';
+
+      test
+        .identify(json.input)
+        .sends(json.output)
+        .expects(200)
+        .end(done)
+    });
+
+    it('should map useragent for safari', function(done){
+      var json = test.fixture('identify-useragent');
+      json.input.context.userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.75.14 (KHTML, like Gecko) Version/7.0.3 Safari/7046A194A';
+      json.output.os_name = 'safari';
+      json.output.key = settings.apiKey;
+      json.output.env = settings.env ? 'p' : 's';
+
+      test
+        .identify(json.input)
+        .sends(json.output)
+        .expects(200)
+        .end(done)
+    });
+
+    it('should fallback to web for useragent', function(done){
+      var json = test.fixture('identify-useragent');
+      json.input.context.userAgent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko';
+      json.output.os_name = 'web';
+      json.output.key = settings.apiKey;
+      json.output.env = settings.env ? 'p' : 's';
+
+      test
+        .identify(json.input)
+        .sends(json.output)
+        .expects(200)
+        .end(done)
     });
   });
 
